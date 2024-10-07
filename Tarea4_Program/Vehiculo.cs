03adcc8dc325f8811552a98352af0459c71f1ea6
Steven357 <stevengonzalez357@gmail.com>
2024-10-07 17:54:42 -0600

    Se crea una clase dentro de la clase Vehiculo que permite gestionar descuentos.

--- Tarea4_Program/Vehiculo.cs
+++ Tarea4_Program/Vehiculo.cs
@@ -18,5 +18,18 @@ namespace Tarea4_Program
             Modelo = modelo;
             Precio = precio;
         }
+
+        public class descuento
+        {
+            public static double AplicarDescuento(double precio, double porcentajeDescuento)
+            {
+                if (porcentajeDescuento < 0 || porcentajeDescuento > 100)
+                    throw new ArgumentException("El porcentaje de descuento debe estar entre 0 y 100");
+
+                return precio - (precio * (porcentajeDescuento / 100));
+
+            }
+
+        }
     }
 }
