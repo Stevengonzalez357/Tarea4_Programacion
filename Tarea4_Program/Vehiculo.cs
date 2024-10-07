23c5d187d3115dacb593159805c7f1f4b7c0634e
Steven357 <stevengonzalez357@gmail.com>
2024-10-07 17:50:57 -0600

    Se inicia creando una clase llamada Vehiculo con sus propiedades y constructor.

---
+++ Tarea4_Program/Vehiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4_Program
+{
+    public class Vehiculo
+    {
+        public String Modelo { get; set; }
+
+        public double Precio { get; set; }
+
+
+        public Vehiculo(String modelo, double precio)
+        {
+            Modelo = modelo;
+            Precio = precio;
+        }
+    }
+}
