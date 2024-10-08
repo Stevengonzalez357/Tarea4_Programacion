9ea48a0e42b6e8759865040d5fd753ed35a95e42
Steven357 <stevengonzalez357@gmail.com>
2024-10-07 18:25:23 -0600

    Se utliza la clase Program y se añade Main que es el metodo principal donde se ejecuta el programa. Se crea un objeto Vehiculo con el modelo y precio.

--- Tarea4_Program/Program.cs
+++ Tarea4_Program/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4_Program
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            // Crear una instancia de Vehiculo
+
+            Vehiculo vehiculo = new Vehiculo("Chevrolet Camaro", 100);
+        }
+
+    }
+}        
+   
+  
