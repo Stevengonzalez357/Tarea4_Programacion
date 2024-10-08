a759034dcca07c705227b151af033c745f31f27a
Steven357 <stevengonzalez357@gmail.com>
2024-10-07 18:07:37 -0600

    La clase estatica ExtensionVehiculo contiene el metodo MostrarInformacion el cual permite mostrar la informacion formateada.

--- Tarea4_Program/ExtensionVehiculo.cs
+++ Tarea4_Program/ExtensionVehiculo.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace Tarea4_Program
 {
-    public class ExtensionVehiculo
+    public static class ExtensionVehiculo
     {
+        // Metodo de extension para formatear la informacion
 
+        public static string MostrarInformacion(this Vehiculo vehiculo)
+        {
+            return
+            $"Modelo: {vehiculo.Modelo}, Precio por dia: {vehiculo.Precio:C}";
+        }
     }
 }
